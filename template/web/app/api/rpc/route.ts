cae108818cda570bbbf9b323a05c075c8f3a7af2
Paul Cramer <paulccramer@gmail.com>
2024-06-06 17:16:59 -0400

    asdf

--- template/web/app/api/rpc/route.ts
+++ template/web/app/api/rpc/route.ts
@@ -1,30 +1,53 @@
+// import { isBase } from '@coinbase/onchainkit';
 import { NextRequest, NextResponse } from 'next/server';
+import { UserOperation } from 'permissionless';
+import { paymasterClient } from '@/utils/paymasterClient';
+// import { isValidAAEntrypoint } from '@coinbase/onchainkit/wallet';
+// import { isWalletASmartWallet } from '@coinbase/onchainkit/wallet';
 
-const rpcUrl = process.env.NEXT_PRIVATE_RPC_URL;
+type PaymasterRequest = {
+  method: string;
+  params: [UserOperation<'v0.6'>, string, string];
+};
 
-export async function POST(req: NextRequest) {
-  if (rpcUrl === undefined) {
-    return NextResponse.json(
-      {},
-      {
-        status: 401,
-        statusText:
-          'You need a RPC URL! Get yours at https://www.coinbase.com/developer-platform/products/base-node?utm_source=boat',
-      },
-    );
-  }
+/**
+ * POST function handles incoming POST requests for sponsoring operations.
+ *
+ * @param {Request} r - The incoming request object.
+ * @returns {Promise<Response>} - The response object containing the result or an error message.
+ */
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  const reqBody: PaymasterRequest = (await req.json()) as PaymasterRequest;
+  const { method, params } = reqBody;
+  const [userOp] = params;
 
-  // forward to Coinbase Developer Platform RPC
-  return fetch(rpcUrl, req)
-    .then(async (response) => {
-      // Return the response data to the client
-      return NextResponse.json(await response.json(), {
-        status: response.status,
-        statusText: response.statusText,
+  // const chainId = 1111;
+
+  // if (!isBase({ chainId })) {
+  //   console.log('The chainId is not Base.');
+  //   return NextResponse.json({ error: 'Invalid chainId' }, { status: 400 });
+  // } else {
+  //   console.log('The chainId is Base.');
+  // }
+
+  try {
+    let result;
+    // Initial checks and preliminary validation.
+    if (method === 'pm_getPaymasterStubData') {
+      result = await paymasterClient.getPaymasterStubData({
+        userOperation: userOp,
       });
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      return NextResponse.json({}, { status: 500, statusText: 'Internal Server Error' });
-    });
+      // Full validation and actual sponsorship data.
+    } else if (method === 'pm_getPaymasterData') {
+      result = await paymasterClient.getPaymasterData({
+        userOperation: userOp,
+      });
+    } else {
+      return NextResponse.json({ error: 'Method not found' }, { status: 404 });
+    }
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    console.error('Error:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
 }
